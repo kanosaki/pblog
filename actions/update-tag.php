fdc7c9ce3c98272b925489e380d589673c205935
kanosaki <kanosaki@gmail.com>
2012-07-16 09:58:37 +0900

    Add update-tag

---
+++ actions/update-tag.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once '../lib/common.php';
+require_once '../models/post.class.php';
+$ret = array(
+    "status" => "Failed"
+);
+if($session->is_logined()){
+    if(isset($_POST['post_id'])){
+        $value = $_POST['value'];
+        $post = Post::find($_POST['post_id']);
+        $post->setTags($value);
+        $ret['status'] = "OK";
+        $ret['msg'] = "Successfully updated!";
+    } else {
+        $ret['msg'] = 'Missing post_id';
+    }
+} else {
+    $ret['msg'] = "You have to sign in.";
+}
+
+echo json_encode($ret);
+
+?>
